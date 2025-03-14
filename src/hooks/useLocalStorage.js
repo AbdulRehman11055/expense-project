7d5e49dd1edba9fefb0f9d2ac06ec7d8e486ec22
Maan Sheikh <ar5124970@gmail.com>
2025-03-15 02:51:45 +0500

    updated local storage code

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,23 +1,61 @@
+// import { useEffect, useState } from "react";
+
+// export function useLocalStorage(key,initialData){
+//    const [data,setData]=useState(initialData)
+//    useEffect(()=>{
+//     const existingData=JSON.parse(localStorage.getItem(key))
+//     if(existingData){
+//         setData(existingData)
+//     }else{
+//         localStorage.setItem(key,JSON.stringify(initialData))
+//     }
+//    },[])
+//    const updateLocalStorage=(newData)=>{
+//     if(typeof newData === "function"){
+//         localStorage.setItem(key,JSON.stringify(newData(data)))
+//     }else{
+//         localStorage.setItem(key,JSON.stringify(newData))
+//     }
+    
+//     setData(newData)
+//    }
+//    return[data,updateLocalStorage]
+// }
+
 import { useEffect, useState } from "react";
 
-export function useLocalStorage(key,initialData){
-   const [data,setData]=useState(initialData)
-   useEffect(()=>{
-    const existingData=JSON.parse(localStorage.getItem(key))
-    if(existingData){
-        setData(existingData)
-    }else{
-        localStorage.setItem(key,JSON.stringify(initialData))
+export function useLocalStorage(key, initialData) {
+  const [data, setData] = useState(initialData);
+
+  useEffect(() => {
+    try {
+      const existingData = localStorage.getItem(key);
+      if (existingData !== null) {
+        setData(JSON.parse(existingData));
+      } else {
+        localStorage.setItem(key, JSON.stringify(initialData));
+      }
+    } catch (error) {
+      console.error("Error reading from localStorage", error);
+      // In case JSON.parse fails, we can just use the initialData
+      setData(initialData);
     }
-   },[])
-   const updateLocalStorage=(newData)=>{
-    if(typeof newData === "function"){
-        localStorage.setItem(key,JSON.stringify(newData(data)))
-    }else{
-        localStorage.setItem(key,JSON.stringify(newData))
+  }, [key, initialData]);
+
+  const updateLocalStorage = (newData) => {
+    try {
+      if (typeof newData === "function") {
+        const updatedData = newData(data);
+        localStorage.setItem(key, JSON.stringify(updatedData));
+        setData(updatedData);
+      } else {
+        localStorage.setItem(key, JSON.stringify(newData));
+        setData(newData);
+      }
+    } catch (error) {
+      console.error("Error writing to localStorage", error);
     }
-    
-    setData(newData)
-   }
-   return[data,updateLocalStorage]
+  };
+
+  return [data, updateLocalStorage];
 }
